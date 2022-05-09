1d5888af85702568541e4173f83837663ff5faa5
Anoopnegiishere <93996623+Anoopnegiishere@users.noreply.github.com>
2022-05-09 18:02:13 +0530

    Create verybigsum.cpp

---
+++ assignment 1/verybigsum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    long sum;
+
+    long arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }cout<<sum;
+    return 0;
+} 
