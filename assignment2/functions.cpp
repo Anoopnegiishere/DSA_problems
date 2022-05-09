a88c92bd787a5758f5e004a3e3c9a62a99d30f3a
Anoopnegiishere <93996623+Anoopnegiishere@users.noreply.github.com>
2022-05-09 18:25:45 +0530

    Create functions.cpp

---
+++ assignment2/functions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    if (a>b&&a>c&&a>d)
+   { cout<<a;
+    return (a);
+}else if(b>a&&b>c&&b>d)
+{
+    cout<<b;
+    return (b);
+}
+    else if(c>a&&c>b&&c>d)
+    {
+        cout<<c;
+        return (c);
+    }
+    
+    else {
+    cout<<d;
+    return (d);
+    }   
+    
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    
+    
+    return 0;
+}
