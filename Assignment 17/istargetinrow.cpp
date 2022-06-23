52be3b9d4fb43fa9f5d17265770655ee6f4a880e
Anoopnegiishere <93996623+Anoopnegiishere@users.noreply.github.com>
2022-06-23 21:43:43 +0530

    Update istargetinrow.cpp

--- Assignment 17/istargetinrow.cpp
+++ Assignment 17/istargetinrow.cpp
@@ -3,26 +3,23 @@ using namespace std;
 
  bool isTragetPresentInRow(vector<vector<int> >A, int target, int r){
     //returns true if target is present in row r of A then returns true else return false;
-     int i=0;
-     while(i<r)
-     {
+     int i=r-1;
       for(int j=0;j<A[0].size();j++)   
       {
-          cout<<A[i][j]<<" ";
          if(A[i][j]==target)
          return true;
      }
-         i++;
-     }
+    
      return false;
 }
 int main()
 {
-    vector<vector<int>>A = { {23,44,71},
+    vector<vector<int>>A = {
+                           {23,44,71},
                            {60,19,24},
                            {51,85,99}
                          };
- bool a = isTragetPresentInRow(A,85, A.size());
+ bool a = isTragetPresentInRow(A,85, 3);
     cout<<a;
     return 0;
 }
