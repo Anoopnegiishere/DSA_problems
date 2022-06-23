1b0c0faad7d09eaeebaf6fdebcd436ce1b12ea8f
Anoopnegiishere <93996623+Anoopnegiishere@users.noreply.github.com>
2022-06-23 21:41:33 +0530

    Update istargetincol.cpp

--- Assignment 17/istargetincol.cpp
+++ Assignment 17/istargetincol.cpp
@@ -3,26 +3,23 @@ using namespace std;
 
  bool isTragetPresentInCol(vector<vector<int> >A, int target, int c){
     //returns true if target is present in row r of A then returns true else return false;
-     int j=0;
-     while(j<c)
-     {
+     int j=c-1;
       for(int i=0;i<A.size();i++)   
       {
-          cout<<A[i][j]<<" ";
          if(A[i][j]==target)
+         
          return true;
-     }
-         j++;
      }
      return false;
 }
 int main()
 {
-    vector<vector<int>>A = { {27,24,92},
+    vector<vector<int>>A = { 
+                           {27,24,92},
                            {67,19,23},
                            {53,15,79}
                          };
- bool a = isTragetPresentInCol(A,85, A[0].size());
+ bool a = isTragetPresentInCol(A,15, 2);
     cout<<a;
     return 0;
 }
