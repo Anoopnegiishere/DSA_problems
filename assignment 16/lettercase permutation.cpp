2c5c7ae73ebd4ec82e237891833be5e45da8f06f
Anoopnegiishere <93996623+Anoopnegiishere@users.noreply.github.com>
2022-06-29 21:13:48 +0530

    Create lettercase permutation.cpp

---
+++ assignment 16/lettercase permutation.cpp
@@ -0,0 +1,21 @@
+ void solve(vector<string>& ans,int index,string s){
+        if(index==s.size()){
+            ans.push_back(s);
+            return ; 
+        }
+           solve(ans,index+1,s);
+        if(isalpha(s[index])){
+         if(s[index]<97){
+             s[index]+=32;
+         }else
+            s[index]-=32;
+         solve(ans,index+1,s);
+        }
+        
+    }
+    vector<string> letterCasePermutation(string s) {
+        vector<string>  ans;
+        solve(ans,0,s);
+        return ans;
+    }
+};
