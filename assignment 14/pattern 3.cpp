9f0a4962639fb427f730b4dc24f8e5a7d0a630bd
Anoopnegiishere <93996623+Anoopnegiishere@users.noreply.github.com>
2022-09-25 20:01:22 +0530

    Create pattern 3.cpp

---
+++ assignment 14/pattern 3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n"<<endl;
+    cin>>n;
+
+    while(n>0)
+    {
+        
+        for(int i=0;i<n%10;i++)
+        cout<<"#";
+        cout<<endl;
+        n /=10;
+    }
+    return 0;
+}
